bb6fe957fc8be52594dc1a8b145e4256afcd07d7
NaboVolador <aleixmingot06@gmail.com>
2026-01-16 10:43:51 +0100

    Santo Cielos omaigad

--- addons/incidentes/__manifest__.py
+++ addons/incidentes/__manifest__.py
@@ -1,35 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "incidentes",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    'name': "Modulo de Partes de Incidentes",
+    'version': '1.0',
+    'depends': [
+        'base',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    'author': "Aleix, Jorge y Adam",
+    'category': 'Partes de Incidentes',
+    'website': "https://foremp.edu.gva.es/",
+    'description': ''' Modulo para hacer gestión de Partes de Incidentes.''',
+    'summary': ' Modulo de Partes de Incidentes',
+    'data': [
+        'views/views.xml',
     ],
 }
