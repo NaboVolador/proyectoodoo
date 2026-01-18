8bf1338f71e68b76ca354580ce53f8da451b9c01
NaboVolador <aleixmingot06@gmail.com>
2026-01-18 19:18:48 +0100

    Sub-menos con atributos funcionales

--- addons/incidentes/models/profesor.py
+++ addons/incidentes/models/profesor.py
@@ -3,3 +3,6 @@ from odoo import fields, models, api
 class Profesor(models.Model):
     _name = 'profesor'
     name = fields.Char()
+
+    _dni = 'Documento Nacional de Identidad'
+    dni = fields.Char()
